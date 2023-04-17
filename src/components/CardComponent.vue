c566cfe265908778f1698ef43464a0e500e38e59
peppeluc22 <gluca0260@gmail.com>
2023-04-17 17:25:04 +0200

    created card

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 p-2">
+        <div class="card">
+            <div class="card-image">
+                <img :src="image" class="w-100">
+            </div>
+
+            <div class="card-body text-center">
+
+
+                <h5 class="text-white text-uppercase">{{ name }}</h5>
+
+                <p class="fw-bold">{{ archetype }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Card',
+    props: ['image', 'name', 'archetype']
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/styles/partials/_variables.scss' as *;
+
+.card-body {
+    background-color: $main-color;
+}
+</style>
